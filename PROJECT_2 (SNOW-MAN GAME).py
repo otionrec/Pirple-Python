d8a8cd925afa1ebeb1c3b916986d566b426b640b
otionrec <otionrec2@gmail.com>
2021-03-30 03:24:28 +0000

    Update PROJECT_2 (SNOW-MAN GAME).py
    enjoy

--- PROJECT_2 (SNOW-MAN GAME).py
+++ PROJECT_2 (SNOW-MAN GAME).py
@@ -1,7 +1,10 @@
 import random
 import turtle
 from turtle import *
-
+from playsound import playsound
+import time
+from multiprocessing import Process, Pipe, Queue
+from threading import Timer
 
 # Drawing of circles
 def draw_circle(turtle, color, x, y, radius):
@@ -30,7 +33,6 @@ def draw_rectangle(turtle, color, x, y, width, height):
     turtle.end_fill()
     turtle.setheading(0)
     
-
 # Number of tries set to 0
 trys = 0
 
@@ -50,85 +52,67 @@ words_list = [
 secret_word = "#####"
 
 
-# Setting up the turtle window
-speed(0)
-turtle.setup(600, 600)
-window = turtle.Screen()
-window.bgcolor("#FFFFFF")
+def timeout():
+    penup()
+    color("purple")
+    goto(-67, 180)
+    write("Delayed!", font=("Arial", 20, "bold"))
+
+    enter = textinput("Wanna play again?", "1) Yes \n2) No")
+    
+    if enter == "1" or enter.casefold() == "yes".casefold() or enter.casefold() == "y".casefold():
+        speed(0)
+        clear()
+        display_game()
+    elif enter == "2" or enter.casefold() == "no".casefold() or enter.casefold() == "n".casefold():
+        exit()   
+    else:
+        textinput("Not A Valid Input", "Try again!")
+        speed(0)
+        clear()        
+        display_game()
+
+    return True
+
+
+def ticking():
+    # duration is in seconds
+    t = Timer(150, timeout)
+    t.start()
+    first_guess(secret_word, trys, no_correct_guesses, first_guessed_letter)
+    # wait for time completion
+    t.join()
+
+    return True
+
 
 # Creating the game
-def display_game(draw_backgrd=True, draw_bottom=True, draw_mid=True, draw_top=True, welcome=True,
-                 button1=False, button2=False, button3=False, lefty=False, righty=False, draw_le=False, draw_re=False,
-                 draw_mouth=False, draw_nose=False, ask=True):
+def display_game():
 
-    if draw_backgrd == True:
-        draw_circle(turtle, "cyan", 0, -200, 220)
+# Setting up the turtle window
+    speed(0)
+    turtle.setup(600, 600)
+    window = turtle.Screen()
+    window.bgcolor("#FFFFFF")
 
-    if draw_bottom == True:
-        draw_circle(turtle, "white", 0, -180, 90)
+    draw_circle(turtle, "cyan", 0, -200, 220)
 
-    if draw_mid == True:
-        draw_circle(turtle, "white", 0, -60, 70)
+    draw_circle(turtle, "white", 0, -180, 90)
 
-    if draw_top == True:
-        draw_circle(turtle, "white", 0, 50, 50)
-        hideturtle()
+    draw_circle(turtle, "white", 0, -60, 70)
 
-    if welcome == True:
-        penup()
-        color("pink")
-        goto(-110, 250)
-        write("Snow-Man Game", font=("Arial", 20, "bold"))
+    draw_circle(turtle, "white", 0, 50, 50)
+    hideturtle()
 
-    if button1 == True:
-        draw_circle(turtle, "black", 0, -80, 7)
-        hideturtle()
+    penup()
+    color("pink")
+    goto(-100, 250)
+    write("Snow-Man Game", font=("Arial", 20, "bold"))
+    
+    askGameType()
 
-    if button2 == True:
-        draw_circle(turtle, "black", 0, -35, 7)
-        hideturtle()
-
-    if button3 == True:
-        draw_circle(turtle, "black", 0, 10, 7)
-        hideturtle()
-
-    if lefty == True:
-        left(45)
-        draw_rectangle(turtle, "brown", 50, 25, 50, 5)
-        left(0)
-        draw_rectangle(turtle, "brown", 75, 50, 20, 3)
-        left(90)
-        draw_rectangle(turtle, "brown", 75, 50, 20, 3)
-        hideturtle()
-
-    if righty == True:
-        right(45)
-        draw_rectangle(turtle, "brown", -90, 55, 50, 5)
-        left(0)
-        draw_rectangle(turtle, "brown", -100, 45, 20, 3)
-        left(90)
-        draw_rectangle(turtle, "brown", -75, 45, 20, 3)
-        hideturtle()
-
-    if draw_le == True:
-        draw_circle(turtle, "black", -15, 110, 7)
-        hideturtle()
-
-    if draw_re == True:
-        draw_circle(turtle, "black", 15, 110, 7)
-        hideturtle()
-
-    if draw_mouth == True:
-        draw_circle(turtle, "red", 0, 80, 12)
-        draw_circle(turtle, "white", 0, 85, 12)
-        hideturtle()
-
-    if draw_nose == True:
-        draw_circle(turtle, "orange", 0, 98, 5)
-        hideturtle()
-
-    if ask == True:
-        askGameType()
+    window.listen()
+    exitonclick()
 
     return True
 
@@ -150,7 +134,7 @@ def buttons(button1=True, button2=True, button3=True):
     return True
 
 # Drawing of Snow-man hands
-def hands(lefty=True, righty=True ):
+def hands(lefty=True, righty=True):
 
     if lefty == True:
         left(45)
@@ -171,6 +155,7 @@ def hands(lefty=True, righty=True ):
         hideturtle()
 
     return True
+
 
 # Drawing of Snow-man eyes
 def eyes(draw_le=True, draw_re =True):
@@ -204,7 +189,6 @@ def nose(draw_nose=True):
 
     return True
 
-
 # hidding of secrewor with the '#' symbol
 def dashed(secret_word):
 
@@ -220,19 +204,23 @@ no_correct_guesses = dashed(secret_word)
 def input_letters(one=False, two=False, three=False, four=False, five=False):
     letter=""
     if one==True:
-        letter = textinput("Guess the first letter", no_correct_guesses)
+        letter = textinput("Guess the First letter", no_correct_guesses)
         return letter
+
     if two==True:
-        letter = textinput("Correct! Make your second guess", first_guessed_letter)
+        letter = textinput("Correct! Second Letter", first_guessed_letter)
         return letter
+
     if three==True:
-        letter = textinput("Good! Guess the third letter", second_guessed_letter)
+        letter = textinput("Good! Third Letter", second_guessed_letter)
         return letter
+
     if four==True:
-        letter = textinput("Great! Now the fourth guess", third_guessed_letter)
+        letter = textinput("Great! Fourth guess", third_guessed_letter)
         return letter
+
     if five==True:
-        letter = textinput("Fantastic! Make your last guess", fourth_guessed_letter)
+        letter = textinput("Fantastic! Last guess", fourth_guessed_letter)
 
     return letter
     
@@ -242,12 +230,12 @@ letter = input_letters()
 # Function for number of guessed letters
 def one_guessed(no_correct_guesses, letter):
     
-    #i = 0
     if letter == secret_word[0]:      
         show_first = no_correct_guesses[0].replace("#", letter)
         return show_first
 
 first_guessed_letter = one_guessed(no_correct_guesses, letter)
+
 
 def two_guessed(first_guessed_letter, letter):
     
@@ -257,6 +245,7 @@ def two_guessed(first_guessed_letter, letter):
 
 second_guessed_letter = two_guessed(first_guessed_letter, letter)
 
+
 def three_guessed(second_guessed_letter, letter):
     
     if letter == secret_word[2]:             
@@ -264,6 +253,7 @@ def three_guessed(second_guessed_letter, letter):
         return show_third
 
 third_guessed_letter = three_guessed(second_guessed_letter, letter)
+
 
 def four_guessed(third_guessed_letter, letter):
     
@@ -273,6 +263,7 @@ def four_guessed(third_guessed_letter, letter):
 
 fourth_guessed_letter = four_guessed(third_guessed_letter, letter)
 
+
 def five_guessed(fourth_guessed_letter, letter):
     
     if letter == secret_word[4]:             
@@ -281,16 +272,19 @@ def five_guessed(fourth_guessed_letter, letter):
 
 fifth_guessed_letter = five_guessed(fourth_guessed_letter, letter)
 
+
 # Functions to allow for guesses
 def first_guess(secret_word, trys, no_correct_guesses, first_guessed_letter):
-
+    ticking()
+    playsound(song, False)
+    
     letter = input_letters(one=True, two=False, three=False, four=False, five=False)
     
-    if letter == secret_word[0] and len(letter) == 1:
+    if letter.casefold() == secret_word[0].casefold() and len(letter) == 1:
 
         penup()
         color("lime")
-        goto(-80, -290)
+        goto(-100, -290)
         write(letter, font=("Arial", 50, "bold"))
 
         trys += 1
@@ -303,7 +297,6 @@ def first_guess(secret_word, trys, no_correct_guesses, first_guessed_letter):
         trys += 1
         textinput("Wrong!", "Hit Enter And Try Again.")
         return first_guess(secret_word, trys, no_correct_guesses, first_guessed_letter)
-
 
     return True
 
@@ -317,7 +310,7 @@ def second_guess(secret_word, trys, first_guessed_letter, second_guessed_letter)
 
         penup()
         color("lime")
-        goto(-40, -290)
+        goto(-60, -290)
         write(letter, font=("Arial", 50, "bold"))
 
         trys += 1
@@ -331,12 +324,11 @@ def second_guess(secret_word, trys, first_guessed_letter, second_guessed_letter)
         textinput("Wrong!", "Hit Enter And Try Again.")
         return second_guess(secret_word, trys, first_guessed_letter, second_guessed_letter)
 
-
     return True
+
 
 def third_guess(secret_word, trys, second_guessed_letter, third_guessed_letter):
     
-
     hands()
     letter = input_letters(one=False, two=False, three=True, four=False, five=False)
 
@@ -344,7 +336,7 @@ def third_guess(secret_word, trys, second_guessed_letter, third_guessed_letter):
 
         penup()
         color("lime")
-        goto(0, -290)
+        goto(-20, -290)
         write(letter, font=("Arial", 50, "bold"))
 
         trys += 1
@@ -358,8 +350,8 @@ def third_guess(secret_word, trys, second_guessed_letter, third_guessed_letter):
         textinput("Wrong!", "Hit Enter And Try Again.")
         return third_guess(secret_word, trys, second_guessed_letter, third_guessed_letter)
 
-
     return True
+
 
 def fourth_guess(secret_word, trys, third_guessed_letter, fourth_guessed_letter):
     
@@ -371,7 +363,7 @@ def fourth_guess(secret_word, trys, third_guessed_letter, fourth_guessed_letter)
 
         penup()
         color("lime")
-        goto(40, -290)
+        goto(20, -290)
         write(letter, font=("Arial", 50, "bold"))
 
         trys += 1
@@ -387,6 +379,7 @@ def fourth_guess(secret_word, trys, third_guessed_letter, fourth_guessed_letter)
 
     return True
 
+
 def fifth_guess(secret_word, trys, fourth_guessed_letter, fifth_guessed_letter):
     
     mouth()
@@ -396,7 +389,7 @@ def fifth_guess(secret_word, trys, fourth_guessed_letter, fifth_guessed_letter):
 
         penup()
         color("lime")
-        goto(80, -290)
+        goto(60, -290)
         write(letter, font=("Arial", 50, "bold"))
 
         trys += 1
@@ -410,31 +403,63 @@ def fifth_guess(secret_word, trys, fourth_guessed_letter, fifth_guessed_letter):
         textinput("Wrong!", "Hit Enter And Try Again.")
         return fifth_guess(secret_word, trys, fourth_guessed_letter, fifth_guessed_letter)
 
-
     return True
+
 
 def completed(trys):
 
     nose()
     trys_str = str(trys)
 
-    penup()
-    color("purple")
-    goto(-100, 180)
-    write("Congratulations", font=("Arial", 20, "bold"))
+    if len(trys_str) == 1:
+        penup()
+        color("purple")
+        goto(-67, 180)
+        write("Well-done!", font=("Arial", 20, "bold"))
 
-    penup()
-    color("yellow")
-    goto(-72, 150)
-    write("After " + trys_str + " trys", font=("Arial", 20, "bold"))
+        penup()
+        color("yellow")
+        goto(-80, 150)
+        write("After ", font=("Arial", 20, "bold"))
 
-    enter = textinput("Do you wanna play again?", "1) Yes \n2) No")
+        penup()
+        color("brown")
+        goto(0, 150)
+        write(trys_str, font=("Arial", 20, "bold"))
+
+        penup()
+        color("yellow")
+        goto(30, 150)
+        write("trys", font=("Arial", 20, "bold"))        
+
+    elif len(trys_str)>=2:
+        penup()
+        color("purple")
+        goto(-67, 180)
+        write("Well-done!", font=("Arial", 20, "bold"))
+
+        penup()
+        color("yellow")
+        goto(-80, 150)
+        write("After ", font=("Arial", 20, "bold"))
+
+        penup()
+        color("brown")
+        goto(-10, 150)
+        write(trys_str, font=("Arial", 20, "bold"))
+
+        penup()
+        color("yellow")
+        goto(30, 150)
+        write("trys", font=("Arial", 20, "bold"))
+
+    enter = textinput("Wanna play again?", "1) Yes \n2) No")
     
-    if enter == "1" or enter.casefold() == "yes".casefold():
+    if enter == "1" or enter.casefold() == "yes".casefold() or enter.casefold() == "y".casefold():
         speed(0)
         clear()
         display_game()
-    elif enter == "2" or enter.casefold() == "no".casefold():
+    elif enter == "2" or enter.casefold() == "no".casefold() or enter.casefold() == "n".casefold():
         exit()   
 
     else:
@@ -450,7 +475,6 @@ def askComputerToPick():
     secret_word = random.choice(words_list)
     return secret_word.lower()
 
-
 # Asking Host to pick word
 def askHostToTypeSecretWord(repeatText=None):
     question = repeatText if repeatText is not None else "Type in your secret 5-letter word or 'exit' enter 'back' for Menu.\n"
@@ -459,9 +483,9 @@ def askHostToTypeSecretWord(repeatText=None):
 
     if type(secret_word) == str and len(secret_word) == 5:
         return secret_word.lower()
-    elif secret_word == 'back':
+    elif secret_word.casefold() == 'back'.casefold():
         return askGameType()
-    elif secret_word == "exit":
+    elif secret_word.casefold() == "exit".casefold():
         exit()
     else:
         return askHostToTypeSecretWord("Please the word should be exactly 5 letters! Type again or 'back' for Main Menu.\n")
@@ -481,7 +505,7 @@ def askGameType():
         first_guess(secret_word, trys, no_correct_guesses, first_guessed_letter)
         return secret_word
 
-    elif game_type == "exit" or game_type == "3":
+    elif game_type.casefold() == "exit".casefold() or game_type == "3":
         exit()
 
     else:
@@ -489,9 +513,5 @@ def askGameType():
 
     return True
 
-# initializing the game
+
 display_game()
-
-window.listen()
-exitonclick()
-
